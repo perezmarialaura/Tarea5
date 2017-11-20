d52df1a5c824ef80af13cd89cfc72dd1ba39f89d
Maria Laura <ml.perez11@uniandes.edu.co>
2017-11-19 19:36:43 -0500

    arreglo likelihood y arrays

--- CurvaRotacion.c
+++ CurvaRotacion.c
@@ -15,16 +15,16 @@ double get_random(void);
 
 int main(void){
   srand(time(NULL)); //sin esto, el rand no cambia al compilar varias veces
-  double mb_new, md_new, mh_new, alpha, beta, l_new, l_old;
-  float deltab, deltad, deltah;
-  double *radius = malloc(N*sizeof(double));
-  double *velocities = malloc(N*sizeof(double));
-  double *v_old = malloc(N*sizeof(double));
-  double *v_new = malloc(N*sizeof(double));
+  double mb_prime, md_prime, mh_prime, alpha, beta, l_prime, l_init, delta;
+  double *r_obs = malloc(N*sizeof(double));
+  double *v_obs = malloc(N*sizeof(double));
+  double *v_init = malloc(N*sizeof(double));
+  double *v_prime = malloc(N*sizeof(double));
   double *mb_walk = malloc(n*sizeof(double));
   double *md_walk = malloc(n*sizeof(double));
   double *mh_walk = malloc(n*sizeof(double));
   double *l_walk = malloc(n*sizeof(double));
+
   FILE *data = fopen("RadialVelocities.dat", "r");
   int i;
   //elimino la primera fila
@@ -32,43 +32,48 @@ int main(void){
   fgets(buffer, 100, data);
   //e importo los datos
   for (i = 0; i < N; i++){
-    fscanf(data, "%lf %lf \n", &radius[i], &velocities[i]);
+    fscanf(data, "%lf %lf \n", &r_obs[i], &v_obs[i]);
   }
   fclose(data);
   //inicializo valores
   mb_walk[0] = get_random()*1000;
   md_walk[0] = get_random()*10000;
   mh_walk[0] = get_random()*100000;
-  v_old = model(radius, mb_walk[0], md_walk[0], mh_walk[0]);
-  l_walk[0] = likelihood(velocities, v_old);
-  deltab = 50;
-  deltad = 500;
-  deltah = 1000;
-  for (i = 0; i < n; i++){
-      mb_new = mb_walk[i] + 2*deltab*(get_random()-0.5);
-      md_new = md_walk[i] + 2*deltad*(get_random()-0.5);
-      mh_new = mh_walk[i] + 2*deltah*(get_random()-0.5);
-      v_old = model(radius, mb_walk[i], md_walk[i], mh_walk[i]);
-      v_new = model(radius, mb_new, md_new, mh_new);
-      l_new = likelihood(velocities, v_new);
-      l_old = likelihood(velocities, v_old);
-      alpha = l_new/l_old;
+
+  v_init = model(r_obs, mb_walk[0], md_walk[0], mh_walk[0]);
+  l_walk[0] = likelihood(v_obs, v_init);
+  delta = 100;
+  for (i = 1; i < n; i++){
+      mb_prime = mb_walk[i-1] + 2*delta*(get_random()-0.5);
+      md_prime = md_walk[i-1] + 2*delta*(get_random()-0.5);
+      mh_prime = mh_walk[i-1] + 2*delta*(get_random()-0.5);
+
+      v_init = model(r_obs, mb_walk[i-1], md_walk[i-1], mh_walk[i-1]);
+      v_prime = model(r_obs, mb_prime, md_prime, mh_prime);
+
+      l_prime = likelihood(v_obs, v_prime);
+      l_init = likelihood(v_obs, v_init);
+
+      alpha = l_prime/l_init;
       if(alpha >= 1.0){
-        mb_walk[i] = mb_new;
-        md_walk[i] = md_new;
-        mh_walk[i] = mh_new;
-        l_walk[i] = l_new;
+        mb_walk[i] = mb_prime;
+        md_walk[i] = md_prime;
+        mh_walk[i] = mh_prime;
+        l_walk[i] = l_prime;
       }
       else{
         beta = get_random();
         if(alpha >= beta){
-          mb_walk[i] = mb_new;
-          md_walk[i] = md_new;
-          mh_walk[i] = mh_new;
-          l_walk[i] = l_new;
+          mb_walk[i] = mb_prime;
+          md_walk[i] = md_prime;
+          mh_walk[i] = mh_prime;
+          l_walk[i] = l_prime;
         }
         else{
-          l_walk[i] = l_old;
+          mb_walk[i] = mb_walk[i-1];
+          md_walk[i] = md_walk[i-1];
+          mh_walk[i] = mh_walk[i-1];
+          l_walk[i] = l_init;
         }
       }
     }
@@ -88,7 +93,7 @@ double likelihood(double *v_obs, double *v_model){
   for(i = 0; i < N; i++){
     chi_squared += 0.5*pow(v_obs[i]-v_model[i],2);
   }
-  return exp(-chi_squared);
+  return exp(-chi_squared/1000.);
 }
 
 double *model(double *R_obs, double Mb, double Md, double Mh){
