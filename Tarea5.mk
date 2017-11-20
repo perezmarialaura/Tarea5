87d1f7785d9005cacc388417c373dc0e7858bdc6
Maria Laura <ml.perez11@uniandes.edu.co>
2017-11-20 11:40:11 -0500

    elimina anteriores pdf

--- Tarea5.mk
+++ Tarea5.mk
@@ -1,14 +1,10 @@
-*.pdf: *.png
+*.pdf:*.png
 	pdflatex Results_hw5.tex
-
-*.png: M.dat
+*.png:M.dat
 	python Plots.py
-
 M.dat:compila
 	./a.out
-
-compila: RadialVelocities.dat
-	gcc -lm CurvaRotacion.c
-
+compila:RadialVelocities.dat
+	gcc CurvaRotacion.c -lm
 clean:
-	rm -f M.dat *.png a.out *.aux *.log
+	rm -f M.dat *.png a.out *.aux *.log *.pdf
